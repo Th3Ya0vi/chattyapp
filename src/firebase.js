25516b763ece7d97cc7ee72da53c75cd385511d9
Yaovi D. Kpoga <dkpoga@gmail.com>
2024-02-26 14:45:25 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage, ref } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDgfpTRTGL1O0kFLtxj8BYDIiUdyr2GuA",
-  authDomain: "chatty-3c4d1.firebaseapp.com",
-  projectId: "chatty-3c4d1",
-  storageBucket: "chatty-3c4d1.appspot.com",
-  messagingSenderId: "82084718834",
-  appId: "1:82084718834:web:5d9219bbc4516d7497486b",
-  measurementId: "G-S2K64JZ1YK"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
